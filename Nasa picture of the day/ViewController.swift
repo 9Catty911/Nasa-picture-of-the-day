6cc0670f414f05b0889be05f84e539b8d9f30c81
Programming <programming@macbook-pro-2.local>
2019-05-25 15:08:07 +0300

    Initial Commit

---
+++ Nasa picture of the day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nasa picture of the day
+//
+//  Created by Programming on 25.05.2019.
+//  Copyright © 2019 Programming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
